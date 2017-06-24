a3b3aac3d74f02ae59586e4aff8234b017d7c23c
JuliaHwang <qufskan9396@gmail.com>
2017-06-25 00:48:54 +0900

    Added the first functional-test comments, and used unittest"

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,6 +1,23 @@
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        # 암묵적 대기 기능 추가 - 3초 대기 후 처리
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
