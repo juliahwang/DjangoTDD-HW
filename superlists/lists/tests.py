f5e384d9bd8603a97542c0c428d37170d03ee18d
JuliaHwang <qufskan9396@gmail.com>
2017-06-25 05:15:34 +0900

    change render to redirect so it help saving the input contents dynamically

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -23,25 +23,54 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        # HttpRequest 객체를 생성해 어떤 요청을 브라우저에 보내는지 확인
+    def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
-        request.method = 'POST'
+        request.method = "POST"
         request.POST['item_text'] = '신규 작업 아이템'
-        # home_page 뷰에 전달해서 HttpResponse 객체로 응답을 받는다.
+
         response = home_page(request)
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        expected_html = render_to_string(
-            'lists/home.html',
-            {
-                'new_item_text': '신규 작업 아이템',
-            }
-        )
-        # decode()를 사용해 expected_html의 byte 데이터를 파이썬 유니코드 문자열로 변환.
-        self.assertEqual(
-            re.sub(self.pattern_input_csrf, '', response.content.decode()),
-            re.sub(self.pattern_input_csrf, '', expected_html)
-        )
+        # 새 아이템이 데이터베이스에 저장되었는지 확인
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        # 아이템 텍스트가 같은지 확인
+        self.assertEqual(new_item.text, '신규 작업 아이템')
+
+        # 응답이 html 에 의해 렌더링되지 않고 redirect되므로 이를 확인하는 코드로 수정.
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+        # self.assertIn('A new list item', response.content.decode())
+        # expected_html = render_to_string(
+        #     'lists/home.html',
+        #     {
+        #         'new_item_text': 'A new list item',
+        #     }
+        # )
+        # self.assertEqual(
+        #     re.sub(self.pattern_input_csrf, '', response.content.decode()),
+        #     re.sub(self.pattern_input_csrf, '', expected_html)
+        # )
+
+
+    # def test_home_page_returns_correct_html(self):
+    #     # HttpRequest 객체를 생성해 어떤 요청을 브라우저에 보내는지 확인
+    #     request = HttpRequest()
+    #     request.method = 'POST'
+    #     request.POST['item_text'] = '신규 작업 아이템'
+    #     # home_page 뷰에 전달해서 HttpResponse 객체로 응답을 받는다.
+    #     response = home_page(request)
+    #     self.assertIn('신규 작업 아이템', response.content.decode())
+    #     expected_html = render_to_string(
+    #         'lists/home.html',
+    #         {
+    #             'new_item_text': '신규 작업 아이템',
+    #         }
+    #     )
+    #     # decode()를 사용해 expected_html의 byte 데이터를 파이썬 유니코드 문자열로 변환.
+    #     self.assertEqual(
+    #         re.sub(self.pattern_input_csrf, '', response.content.decode()),
+    #         re.sub(self.pattern_input_csrf, '', expected_html)
+    #     )
 
         ######### 상수/문자열은 테스트 범위가 아니므로 위처럼 변경 ############
         # # byte형 데이터인 <html></html>로 구성되어 있는지 확인
@@ -50,8 +79,14 @@ class HomePageTest(TestCase):
         # self.assertIn(b'<title>To-Do Lists</title>', response.content)
         # self.assertTrue(response.content.endswith(b'</html>'))
 
-class ItemModelTest(TestCase):
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
+
+class ItemModelTest(TestCase):
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
     def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
@@ -68,3 +103,4 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,11 +1,19 @@
 # Create your views here.
 # home_page = None
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from lists.models import Item
 
 
 def home_page(request):
     # return HttpResponse('<html><title>To-Do Lists</title></html>')
+    if request.method == "POST":
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+    else:
+        new_item_text = ''
     context = {
-        'new_item_text': request.POST.get('item_text'),
+        'new_item_text': new_item_text,
     }
     return render(request, 'lists/home.html', context)
