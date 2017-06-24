9410d6347a6a684b20290d55a489ea4cfd9b30de
JuliaHwang <qufskan9396@gmail.com>
2017-06-25 01:48:22 +0900

    applied the first unit test with url mapping and simple home_page view

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,7 +1,17 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+
+from .views import home_page
 
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+# class SmokeTest(TestCase):
+#     def test_bad_maths(self):
+#         self.assertEqual(1 + 1, 3)
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_homepage_view(self):
+        # resolve('/') : 장고의 내부함수로 url을 해석해서 일치하는 뷰함수를 찾는다.
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
